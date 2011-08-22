19eaaef1f2bd0ee277ddbab223107e4a3b097868
Craig Slusher <cslush@gmail.com>
2011-08-21 19:38:14 -0700

    added ProtectedView to require login at the class-based view level

--- dzen/django/apps/common/views.py
+++ dzen/django/apps/common/views.py
@@ -1,5 +1,16 @@
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.generic import TemplateView
+
 def handler404(request):
     pass
 
 def handler500(request):
     pass
+
+class ProtectedView(TemplateView):
+    template_name = 'secret.html'
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(ProtectedView, self).dispatch(*args, **kwargs)
