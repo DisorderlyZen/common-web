e94bd90102f7c041bd0d2eb45dd3128b75e26c1c
Craig Slusher <cslush@gmail.com>
2011-08-22 22:32:08 -0700

    added JSONResponseMixin

--- dzen/django/apps/common/views.py
+++ dzen/django/apps/common/views.py
@@ -1,4 +1,6 @@
+from django import http
 from django.contrib.auth.decorators import login_required
+from django.utils import simplejson as json
 from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
 
@@ -14,3 +16,22 @@ class ProtectedView(TemplateView):
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
         return super(ProtectedView, self).dispatch(*args, **kwargs)
+
+class JSONResponseMixin(object):
+    def render_to_response(self, context):
+        "Returns a JSON response containing 'context' as payload"
+        return self.get_json_response(self.convert_context_to_json(context))
+
+    def get_json_response(self, content, **httpresponse_kwargs):
+        "Construct an `HttpResponse` object."
+        return http.HttpResponse(content,
+                                 content_type='application/json',
+                                 **httpresponse_kwargs)
+
+    def convert_context_to_json(self, context):
+        "Convert the context dictionary into a JSON object"
+        # Note: This is *EXTREMELY* naive; in reality, you'll need
+        # to do much more complex handling to ensure that arbitrary
+        # objects -- such as Django model instances or querysets
+        # -- can be serialized as JSON.
+        return json.dumps(context)
