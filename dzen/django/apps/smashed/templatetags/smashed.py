a29db2f789ada7394cecd88e44046440adae2e19
Craig Slusher <cslush@gmail.com>
2011-08-28 21:17:14 -0700

    updates to separate css from js

--- dzen/django/apps/smashed/templatetags/smashed.py
+++ dzen/django/apps/smashed/templatetags/smashed.py
@@ -6,39 +6,61 @@ from django.template.defaultfilters import stringfilter
 
 register = template.Library()
 
-SMASH_CONTEXT_NAME = 'smashed_scripts'
-SMASH_CONTEXT_API_KEY = 'smashed_scripts_api_key'
+SMASH_CONTEXT_STYLE = 'smashed_context:style'
+SMASH_CONTEXT_SCRIPT = 'smashed_context:script'
+SMASH_CONTEXT_API_KEY = 'smashed_context:api_key'
+
+@register.simple_tag(takes_context=True)
+def smash_local_style(context, url):
+    return smash_local_resource(context, url, SMASH_CONTEXT_STYLE)
+
+@register.simple_tag(takes_context=True)
+def smash_remote_style(context, url):
+    return smash_remote_resource(context, url, SMASH_CONTEXT_STYLE)
+
+@register.simple_tag(takes_context=True)
+def smash_local_script(context, url):
+    return smash_local_resource(context, url, SMASH_CONTEXT_SCRIPT)
+
+@register.simple_tag(takes_context=True)
+def smash_remote_script(context, url):
+    return smash_remote_resource(context, url, SMASH_CONTEXT_SCRIPT)
+
+def smash_local_resource(context, url, context_name):
+    url = settings.STATIC_URL + url.lstrip('/')
+    return smash_remote_resource(context, url, context_name)
+
+def smash_remote_resource(context, url, context_name):
+    return smash_context(object(), context, url, context_name)
 
 @register.filter
 @stringfilter
 def strip_blank_lines(value):
     return '\n'.join([line for line in value.split('\n') if line.strip()])
 
-@register.simple_tag(takes_context=True)
-def smash_local(context, url):
-    url = '%s%s' % (settings.STATIC_URL, url.lstrip('/'))
-    return smash_context(object(), context, url)
+def smash_context(self, context, url, context_name):
+    if context_name not in context.render_context:
+        context.render_context[context_name] = [OrderedDict()]
 
-@register.simple_tag(takes_context=True)
-def smash_remote(context, url):
-    return smash_context(object(), context, url)
-
-def smash_context(self, context, script_url):
-    if SMASH_CONTEXT_NAME not in context.render_context:
-        context.render_context[SMASH_CONTEXT_NAME] = [OrderedDict()]
-
-    smash_context = context.render_context[SMASH_CONTEXT_NAME][-1]
+    smash_context = context.render_context[context_name][-1]
 
     if self not in smash_context:
         smash_context[self] = []
 
-    smash_context[self].append(script_url)
+    smash_context[self].append(url)
 
     return ''
 
 @register.simple_tag(takes_context=True)
-def smash_flush(context):
-    context.render_context[SMASH_CONTEXT_NAME].append(OrderedDict())
+def smash_flush_styles(context):
+    return smash_flush(context, SMASH_CONTEXT_STYLE)
+
+@register.simple_tag(takes_context=True)
+def smash_flush_scripts(context):
+    return smash_flush(context, SMASH_CONTEXT_SCRIPT)
+
+def smash_flush(context, context_name):
+    context.render_context[context_name].append(OrderedDict())
     return ''
 
 @register.simple_tag(takes_context=True)
@@ -47,9 +69,9 @@ def smash_key(context, api_key):
     return ''
 
 @register.simple_tag(takes_context=True)
-def smash_render(context, var_name=None):
+def smash_render_scripts(context, var_name=None):
     t = template.loader.get_template('smashed/client.html')
-    rendered_template = t.render(Context(render_context(context, var_name)))
+    rendered_template = t.render(Context(render_context(context, var_name, SMASH_CONTEXT_SCRIPT)))
 
     if var_name:
         context[var_name] = rendered_template
@@ -57,14 +79,14 @@ def smash_render(context, var_name=None):
 
     return rendered_template
 
-def render_context(context, var_name):
+def render_context(context, var_name, context_name):
     def resource_list(resources):
         return [script for key in resources for script in resources[key]]
 
-    smash_context = context.render_context.get(SMASH_CONTEXT_NAME, [])
+    smash_context = context.render_context.get(context_name, [])
     resource_set = [resource_list(resources) for resources in smash_context]
 
-    api_key = context.render_context.get(SMASH_CONTEXT_API_KEY, getattr(settings, 'WESUMO_APP_KEY', None))
+    api_key = context.render_context.get(context_name, getattr(settings, 'WESUMO_APP_KEY', None))
 
     if not api_key:
         debug = True
