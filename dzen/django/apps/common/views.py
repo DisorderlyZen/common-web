7eae792ae5b8337757935da01e6572085d3ea262
Craig Slusher <cslush@gmail.com>
2011-12-14 10:59:34 -0800

    Adds CSRFExceptMixin for handling view exemption
    Updated the HttpResponseNotAuthorized to include optional content in the
    response.

--- dzen/django/apps/common/views.py
+++ dzen/django/apps/common/views.py
@@ -2,6 +2,7 @@ from django import http
 from django.contrib.auth.decorators import login_required
 from django.utils import simplejson as json
 from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import View
 
 def handler404(request):
@@ -10,15 +11,20 @@ def handler404(request):
 def handler500(request):
     pass
 
+class CSRFExemptMixin(object):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, *args, **kwargs):
+        return super(CSRFExemptMixin, self).dispatch(*args, **kwargs)
+
 class ProtectedViewMixin(object):
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
         return super(ProtectedViewMixin, self).dispatch(*args, **kwargs)
 
 class JSONResponseMixin(object):
-    def render_to_response(self, context):
+    def render_to_response(self, context, **kwargs):
         "Returns a JSON response containing 'context' as payload"
-        return self.get_json_response(self.convert_context_to_json(context))
+        return self.get_json_response(self.convert_context_to_json(context), **kwargs)
 
     def get_json_response(self, content, **httpresponse_kwargs):
         "Construct an `HttpResponse` object."
--- dzen/django/http.py
+++ dzen/django/http.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 class HttpResponseNotAuthorized(HttpResponse):
     status_code = 401
 
-    def __init__(self, request):
-        super(HttpResponseNotAuthorized, self).__init__()
+    def __init__(self, request, content=''):
+        super(HttpResponseNotAuthorized, self).__init__(content)
         self['WWW-Authenticate'] = 'Basic realm="{}"'.format(get_current_site(request).name)
