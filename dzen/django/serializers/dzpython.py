8a56b8046eb3ec588335aca685968463d313c57c
Craig Slusher <cslush@gmail.com>
2011-12-30 15:52:24 -0800

    finalize object after serialization

--- dzen/django/serializers/dzpython.py
+++ dzen/django/serializers/dzpython.py
@@ -3,8 +3,28 @@ from dzen.django.serializers import SerializerExclusionMixin
 
 class Serializer(SerializerExclusionMixin, PythonSerializer):
     def serialize(self, queryset, **options):
-        options['use_natural_keys'] = True
+        if 'use_natural_keys' not in options:
+            options['use_natural_keys'] = True
+
         return super(Serializer, self).serialize(queryset, **options)
 
     def end_object(self, obj):
-        self.objects.append(self._current)
+        super(Serializer, self).end_object(obj)
+        self.finalize_object(self.objects[-1])
+
+    def finalize_object(self, obj):
+        if self.options.pop('simple_object', True):
+            obj = self.simplify_object(obj)
+
+        if not self.options.pop('allow_empty', False):
+            obj = self.strip_empty(obj)
+
+        self.objects[-1] = obj
+
+    def simplify_object(self, obj):
+        new = obj['fields']
+        new['id'] = obj['pk']
+        return new
+
+    def strip_empty(self, obj):
+        return dict((key, val) for key, val in obj.iteritems() if val not in ('', None, []))
