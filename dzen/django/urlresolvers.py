5aca09e48b8f8d86511cf6cf0132465f392e7dd5
Craig Slusher <cslush@gmail.com>
2012-02-15 22:04:29 -0800

    import reverse always

--- dzen/django/urlresolvers.py
+++ dzen/django/urlresolvers.py
@@ -1,6 +1,7 @@
+from django.core.urlresolvers import reverse
+
 try:
     from django.core.urlresolvers import reverse_lazy
 except ImportError:
-    from django.core.urlresolvers import reverse
     from django.utils.functional import lazy
     reverse_lazy = lazy(reverse, str)
