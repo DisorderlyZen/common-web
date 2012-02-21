915dd7941589326eb81e4d0e430dc2c72f8e695f
Zak Zajac <zak@madzak.com>
2012-02-21 15:56:48 -0800

    added prefix function to nav active_url template tag

--- dzen/django/apps/common/templatetags/nav.py
+++ dzen/django/apps/common/templatetags/nav.py
@@ -4,7 +4,13 @@ from django.core.urlresolvers import reverse
 register = template.Library()
 
 @register.simple_tag
-def active(request, url):
-    if request.path == reverse(url):
+def active_url(request, url, *args):
+    if request.path == reverse(url, args=args):
+        return 'active'
+    return ''
+
+@register.simple_tag
+def active_url_prefix(request, url, *args):
+    if request.path.startswith(reverse(url, args=args)):
         return 'active'
     return ''
